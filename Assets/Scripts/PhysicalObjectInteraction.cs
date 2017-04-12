a5bf70b84f41e1eba7ccb92fbfef74bf5ef340e8
tomalama <tommy.nguyen7@hotmail.com>
2017-04-12 09:59:22 -0400

    Initial commit

---
+++ Assets/Scripts/PhysicalObjectInteraction.cs
@@ -0,0 +1,54 @@
+﻿// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class PhysicalObjectInteraction : MonoBehaviour
+{
+    private Vector3 startingPosition;
+
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+
+    void Start()
+    {
+        startingPosition = transform.localPosition;
+        SetGazedAt(false);
+    }
+
+    void LateUpdate()
+    {
+        GvrViewer.Instance.UpdateState();
+        if (GvrViewer.Instance.BackButtonPressed)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+    }
+
+    public void Reset()
+    {
+        transform.localPosition = startingPosition;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# VR-Minigame-Frenzy
